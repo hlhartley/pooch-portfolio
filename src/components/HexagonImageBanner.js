ed6687eecdcf9994d1552916dd1cb4a001117149
Heather Hartley <hlhartley@gmail.com>
2020-01-15 23:56:42 -0700

    Make hexagon imagages into a master hexagon shape

--- src/components/HexagonImageBanner.js
+++ src/components/HexagonImageBanner.js
@@ -4,12 +4,15 @@ function HexagonImageBanner() {
     return (
         <Fragment>
             <ul id="grid" className="clear">
+              <div className="row">
                 <li>
                     <div className="hexagon lab-with-rose-img"></div>
                 </li>
                 <li>
                     <div className="hexagon dog-playing-with-rope-img"></div>
                 </li>
+              </div>
+              <div className="row">
                 <li>
                     <div className="hexagon dog-with-glasses-img"></div>
                 </li>
@@ -17,29 +20,17 @@ function HexagonImageBanner() {
                     <div className="hexagon dog-with-party-hat-img"></div>
                 </li>
                 <li>
-                    <div className="hexagon dog-and-book-img"></div>
-                </li>
-                <li>
-                    <div className="hexagon dog-and-pie-img"></div>
-                </li>
-                <li>
-                    <div className="hexagon dog-wearing-glasses-img"></div>
-                </li>
-                <li>
                     <div className="hexagon dog-jumping-img"></div>
                 </li>
-                <li>
-                    <div className="hexagon dog-with-frisby-img"></div>
-                </li>
-                <li>
-                    <div className="hexagon corgi-dog-img"></div>
-                </li>
+              </div>
+              <div className="row">
                 <li>
                     <div className="hexagon dog-with-window-down-img"></div>
                 </li>
                 <li>
                     <div className="hexagon husky-dog-img"></div>
                 </li>
+              </div>
             </ul>
         </Fragment>
     );
