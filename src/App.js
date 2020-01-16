13bb2331587bda08d5568cb0d57127d26112bb3b
Heather Hartley <hlhartley@gmail.com>
2020-01-16 16:55:06 -0700

    Add custom Google font, add TopNavigationBar component, add linear gradient background for banner, and change banner syling

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './App.css';
 import './CSS/main.scss';
 import HexagonImageBanner from './components/HexagonImageBanner';
+import TopNavigationBar from './components/TopNavigationBar';
 
 function App() {
   return (
     <div className="App">
+      <TopNavigationBar />
       <HexagonImageBanner />
+      <div>
+        BODY
+      </div>
     </div>
   );
 }
